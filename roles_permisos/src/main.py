b89def52ea16aeffd8fd0a753b4edeb85ca20f18
sneyder53 <sneyderamado.com@gmail.com>
2024-10-10 20:43:26 -0500

    se funaliza back de rooles y permiso

--- roles_permisos/src/main.py
+++ roles_permisos/src/main.py
@@ -2,6 +2,7 @@ from fastapi import Body, FastAPI, status, Depends
 from starlette.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from operator import itemgetter
 from .database import database
 from .models import models
 from .schemas import schemas
@@ -23,6 +24,21 @@ app.add_middleware(
 def root():
     return RedirectResponse(url="/docs/")
 
+@app.get("/role/ping", status_code=status.HTTP_200_OK)
+def verify_health():
+    return {"msg": "Pong"}
+
+@app.get("/role/{role_id}", status_code=status.HTTP_200_OK)
+def getRole(role_id:int, db: Session = Depends(database.get_db)):
+    new_role:models.Role = tasks.findRoleById(db=db,role_id=role_id)
+    if not new_role:
+        return utility.get_json_response('E404', 'El role no existe') 
+    else:
+        return {
+            "ID":new_role.ID,
+            "NOMBRE":new_role.NOMBRE,
+            "PERMISOS":new_role.PERMISOS
+        }
 
 @app.post("/role", status_code=status.HTTP_201_CREATED)
 def create(role: schemas.role = Body(default=None), db: Session = Depends(database.get_db)):
@@ -30,16 +46,15 @@ def create(role: schemas.role = Body(default=None), db: Session = Depends(databa
         return utility.get_json_response('E422', 'El body de la petición esta vacio')
     elif not role.nombre:
         return utility.get_json_response('E400', 'El nombre es obligatorio')
+    
+    new_role1:models.Role = tasks.findRoleByName(db=db,role_name=role.nombre)
+    if new_role1:
+        return utility.get_json_response('E400', 'El Role ya existe')
     else:
-        new_role1:models.Role = tasks.findRoleByName(db=db,role_name=role.nombre)
-        if not new_role1:
-            return utility.get_json_response('E400', 'El Role ya existe')
-        
         new_role: models.Role = tasks.createRole(db=db, role=role)
         return {
             "ID": new_role.ID,
-            "NOMBRE": new_role.NOMBRE,
-            "PERMISOS": new_role.PERMISOS
+            "NOMBRE": new_role.NOMBRE
         }
 
 @app.post("/permiso", status_code=status.HTTP_201_CREATED)
@@ -48,31 +63,33 @@ def create(permiso: schemas.permiso = Body(default=None), db: Session = Depends(
         return utility.get_json_response('E422', 'El body de la petición esta vacio')
     elif not permiso.nombre or not permiso.estado:
         return utility.get_json_response('E400', 'El nombre y el estado es obligatorio')
+    
+    new_permiso1:models.Permiso = tasks.findPermisoByName(db=db,permiso_name=permiso.nombre)
+    if new_permiso1:
+        return utility.get_json_response('E400', 'El Permiso ya existe')
     else:
-        new_permiso: models.permiso = tasks.createPermiso(db=db, permiso=permiso)
+        new_permiso: models.Permiso = tasks.createPermiso(db=db, permiso=permiso)
         return {
             "ID": new_permiso.ID,
             "NOMBRE": new_permiso.NOMBRE,
             "ESTADO": new_permiso.ESTADO
         }
 
-@app.post("/role/{role_id}/permisos" , status_code=status.HTTP_201_CREATED)
+@app.post("/role/{role_id}/permiso" , status_code=status.HTTP_201_CREATED)
 def associate_permiso_to_role(role_id:int, permisos: schemas.PermisoUpdate = Body(default=None) , db: Session = Depends(database.get_db)):
-    role = db.query(role).filter(role.ID == role_id).first()
+    role:models.Role = tasks.findRoleById(db=db,role_id=role_id)
     if not role:
         return utility.get_json_response('E404', 'El rol no existe')
     elif not permisos:
         return utility.get_json_response('E422', 'El body de la petición esta vacio')
     else:
-        new_role_permisos = models.role = tasks.associatePermisosToRole(db=db, role=role, permisos=permisos)
+        new_role_permisos = itemgetter('role') (tasks.associatePermisosToRole(db=db, role=role, permisos=permisos))
         return {
             "ID": new_role_permisos.ID,
             "NOMBRE":new_role_permisos.NOMBRE,
             "PERMISOS":new_role_permisos.PERMISOS
-        }
-@app.get("/role/ping", status_code=status.HTTP_200_OK)
-def verify_health():
-    return {"msg": "Pong"}
+            }
+
 
 
 @app.delete("/role/reset", status_code=status.HTTP_200_OK)
--- roles_permisos/src/tasks/tasks.py
+++ roles_permisos/src/tasks/tasks.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import List
 from sqlalchemy.orm import Session
 from ..models import models
 from ..schemas import schemas
 
 
 def createRole(db: Session, role: schemas.role) -> models.Role:
-    role = models.role(
+    role = models.Role(
         NOMBRE=role.nombre.lower(),
     )
     db.add(role)
@@ -14,8 +15,13 @@ def createRole(db: Session, role: schemas.role) -> models.Role:
     return role
 
 def findRoleByName(db: Session, role_name: schemas.role)->models.Role:
-    role = db.query(models.Role).filter(models.Role.NOMBRE == role_name).first()
-    return role
+    role = db.query(models.Role).filter(models.Role.NOMBRE == role_name.lower()).first()
+    return role if role else False
+
+def findRoleById(db: Session, role_id: schemas.role)->models.Role:
+    role = db.query(models.Role).filter(models.Role.ID == role_id).first()
+    return role if role else False
+
 def createPermiso(db: Session, permiso: schemas.permiso) -> models.Permiso:
     permiso = models.Permiso(
         NOMBRE=permiso.nombre.lower(),
@@ -26,12 +32,23 @@ def createPermiso(db: Session, permiso: schemas.permiso) -> models.Permiso:
     db.refresh(permiso)
     return permiso
 
-def associatePermisosToRole(db: Session,role: schemas.role, permisos: schemas.PermisoUpdate) -> models.Role:
-    role.permisos = permisos
-    db.add(role)
+def findPermisoByName(db: Session, permiso_name: schemas.role)->models.Role:
+    permiso = db.query(models.Permiso).filter(models.Permiso.NOMBRE == permiso_name.lower()).first()
+    return permiso if permiso else False
+
+def findPermisoById(db: Session, permiso_id:int)->models.Role:
+    permiso = db.query(models.Permiso).filter(models.Permiso.ID == permiso_id).first()
+    return permiso if permiso else False
+
+def associatePermisosToRole(db: Session,role: models.Role, permisos: schemas.PermisoUpdate) -> models.Role:
+    for permiso_data in permisos.permisos:
+        permiso = findPermisoById(db=db,permiso_id=permiso_data.id)
+        permiso.ROLE_ID = role.ID
+        db.add(permiso)  
+        
     db.commit()
     db.refresh(role)
-    return role
+    return dict(role = role)
 
 def reset_db(db: Session):
     db.query(models.Role).delete()
