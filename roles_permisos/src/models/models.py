0b331932c4e3a037f174be88fecba19f562cf18a
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-10-14 19:43:13 -0500

    se ajusta para que se pueda añadir 1 permiso a muchos roles

--- roles_permisos/src/models/models.py
+++ roles_permisos/src/models/models.py
@@ -1,20 +1,26 @@
-from sqlalchemy import BigInteger, Column, String, Boolean, ForeignKey
+from sqlalchemy import BigInteger, Column, String, Boolean, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from ..database import database
+
+
+role_permiso = Table(
+    'ROLE_PERMISO', database.Base.metadata,
+    Column('ROLE_ID', BigInteger, ForeignKey('ROLE.ID'), primary_key=True),
+    Column('PERMISO_ID', BigInteger, ForeignKey('PERMISO.ID'), primary_key=True)
+)
 
 class Permiso(database.Base):
     __tablename__ = "PERMISO"
     ID = Column(BigInteger, primary_key=True, autoincrement=True)
     NOMBRE = Column(String)
     ESTADO = Column(Boolean)
-    ROLE_ID = Column(BigInteger, ForeignKey('ROLE.ID'))
-    ROLE = relationship("Role", back_populates="PERMISOS")
+    ROLES = relationship("Role", secondary=role_permiso, back_populates="PERMISOS")
 
 class Role(database.Base):
     __tablename__ = "ROLE"
     ID = Column(BigInteger, primary_key=True, autoincrement=True)
     NOMBRE = Column(String)
-    PERMISOS = relationship("Permiso", back_populates="ROLE")
+    PERMISOS = relationship("Permiso", secondary=role_permiso, back_populates="ROLES")
     
     
 database.Base.metadata.create_all(bind=database.engine)
