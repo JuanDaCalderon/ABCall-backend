e860aadf377bd12a22a51032cc8127e18993ad73
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-10-26 18:06:54 -0500

    se le agrega el rol a la listar usuarios

--- usuarios/src/main.py
+++ usuarios/src/main.py
@@ -70,6 +70,7 @@ def getAllUsuarios(db: Session = Depends(database.get_db)):
                 "apellidos": user.apellidos,
                 "direccion": user.direccion,
                 "fechacreacion": user.fechacreacion,
+                "rol": user.roles,
             } for user in usuarios]
 
 @app.get("/usuarios/{role_id}", status_code=status.HTTP_200_OK)
@@ -87,6 +88,7 @@ def getAllUsuarios(role_id:int, db: Session = Depends(database.get_db)):
                 "apellidos": user.apellidos,
                 "direccion": user.direccion,
                 "fechacreacion": user.fechacreacion,
+                "rol": user.roles,
             } for user in usuarios]
 
 
